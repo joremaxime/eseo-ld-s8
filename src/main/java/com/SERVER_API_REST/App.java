d7960ce8946599cff579117da6d8d3637130fbd2
Maxime Joré <maxime.jore@reseau.eseo.fr>
2019-03-26 13:55:06 +0100

    First commit

---
+++ src/main/java/com/SERVER_API_REST/App.java
@@ -0,0 +1,17 @@
+package com.SERVER_API_REST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args ) {
+    	try {
+	        SpringApplication.run(App.class, args);
+	        System.out.println("Application démarée !");
+    	} catch (Exception e) {
+    		System.out.println("Application erreur \n" + e);
+    	}
+    }
+}
---
+++ src/main/java/come/SERVER_API_REST/config/DAOConnection.java
@@ -0,0 +1,51 @@
+package come.SERVER_API_REST.config;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DAOConnection {
+	
+	public static final String CHEMIN_PROPERTIES = "SERVER_API_REST/src/main/resources/application.properties";
+	
+	public Connection getConnection() throws Exception {
+		String[] properties = this.loadProperties();
+		
+		DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+		Connection connect = DriverManager.getConnection("");
+		
+		return connect;
+	}
+	
+	private String[] loadProperties() throws Exception {
+		Properties properties = new Properties();
+		String url;
+		String user;
+		String password;
+
+		ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+		InputStream fichierProperties = classLoader.getResourceAsStream(CHEMIN_PROPERTIES);
+
+		if (fichierProperties == null) {
+			throw new Exception("Fichier de properties introuvable");
+		}
+
+		try {
+			properties.load(fichierProperties);
+			url = properties.getProperty("url");
+			user = properties.getProperty("user");
+			password = properties.getProperty("password");
+		} catch (FileNotFoundException e) {
+			throw new Exception("Fichier non trouvé" + e);
+		} catch (IOException e) {
+			throw new Exception("Impossible de charger le fichier properties " + CHEMIN_PROPERTIES, e);
+		}
+		
+		return new String[] { url, user, password };
+	}
+	
+}
